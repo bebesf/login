8d70409e8449734e4f6494108b571b853b85318c
bebesf <bebes4sure@gmail.com>
2023-02-01 11:38:52 +0200

    This is a ui design for login page

---
+++ app/src/main/java/com/example/loginui/MainActivity.kt
@@ -0,0 +1,100 @@
+package com.example.loginui
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Email
+import androidx.compose.material.icons.filled.Lock
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.semantics.SemanticsProperties.ContentDescription
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.text.input.VisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation.Companion.None
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.loginui.ui.theme.LoginUiTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            LoginUiTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    Greeting()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting() {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ){
+
+
+        Text(text = "Login")
+        var text by remember { mutableStateOf("") }
+        var password by remember { mutableStateOf("") }
+        var passwordVisible by remember { mutableStateOf(false) }
+
+        OutlinedTextField(
+            value = text,
+            label = { Text(text = "Username") },
+            onValueChange = {newText ->
+                text = newText
+            },
+            trailingIcon = {
+                IconButton(onClick = { /*TODO*/ }) {
+                    Icon(imageVector = Icons.Filled.Email, contentDescription = "email Icon")
+                }
+            }
+        )
+
+        OutlinedTextField(
+            value = password,
+            label = { Text(text = "Password") },
+            onValueChange = { newPassword ->
+                password = newPassword
+            },
+            trailingIcon = {
+                IconButton(onClick = { /*TODO*/ }) {
+
+                    Icon(imageVector = Icons.Filled.Lock, contentDescription ="Passowrd Icon" )
+                }
+            },
+            visualTransformation = if (passwordVisible) VisualTransformation.None else PasswordVisualTransformation(),
+        )
+        Button(onClick = { /*TODO*/ },
+            modifier = Modifier.fillMaxWidth(0.7f),
+
+            ) {
+            Text(text = "submit")
+        }
+
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    LoginUiTheme {
+        Greeting()
+    }
+}
